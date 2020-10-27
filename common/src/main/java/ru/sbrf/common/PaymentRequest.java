d5482d61b21920c5bc436826e5bd74f03e23becb
Orlov1302 <orlov1302@mail.ru>
2020-10-27 11:15:28 +0300

    Работающая модель

--- common/src/main/java/ru/sbrf/common/PaymentRequest.java
+++ common/src/main/java/ru/sbrf/common/PaymentRequest.java
@@ -7,7 +7,6 @@ import java.util.Date;
 @Setter
 @Getter
 @EqualsAndHashCode
-//@Value
 public class PaymentRequest {
     private Date date;
     private String numberAccount;
@@ -16,4 +15,4 @@ public class PaymentRequest {
     private Currency currency;
 }
 
-////
+//
